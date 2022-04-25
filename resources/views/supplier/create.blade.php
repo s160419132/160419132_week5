21303cf7cd098dc95fb47f28a2dab89e765d2285
Kevin sihombing <s160419132@student.ubaya.ac.id>
2022-04-25 13:08:56 +0700

    commit week9

---
+++ resources/views/supplier/create.blade.php
@@ -0,0 +1,30 @@
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body> -->
+@extends('layout.conquer2')
+@section('title')
+   Create Supplier
+@endsection
+@section('content') 
+
+<div class="container">
+
+  <form method="POST" action="{{route('suppliers.store')}}">
+      @csrf
+    <div class="form-group">
+      <label for="name">Nama:</label>
+      <input type="name" class="form-control" id="name" placeholder="Enter name" name="name">
+    </div>
+    <button type="submit" class="btn btn-default">Send</button>
+  </form>
+</div>
+@endsection
